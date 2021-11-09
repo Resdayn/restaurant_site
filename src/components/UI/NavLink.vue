4869fba65e2b3f84b0e17697461e8993fee45e1f
Juan Gonzalez <jgonzalez@naver.com>
2021-11-09 22:24:46 +0900

    created

---
+++ src/components/UI/NavLink.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <a href="#" ref="menu" @mouseover="openDropdown" @mouseleave="closeDropdown">{{menuTitle}}</a>
+
+        <section v-if="isOpen">
+            <slot></slot>
+        </section>
+
+    </section>
+</template>
+
+<script>
+export default {
+    props: ["menuTitle"],
+    data() {
+        return {
+            isOpen: false
+        }
+    },
+    methods: {
+        openDropdown() {
+            this.isOpen = true;
+        },
+        closeDropdown() {
+            this.isOpen = false;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
