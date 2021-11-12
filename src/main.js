de3d6295b9fd0c14a2911dca53372801bc87e88e
Juan Gonzalez <jgonzalez@naver.com>
2021-11-12 18:00:49 +0900

    added BaseButton

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import TheNav from './components/UI/TheNav.vue'
+import BaseButton from './components/Base/BaseButton';
 
 const app = createApp(App);
 
 app.component('the-nav', TheNav);
+app.component('base-button', BaseButton);
 
 app.use(router).mount('#app');
