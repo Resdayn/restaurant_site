fe9067fc8c53b1dc2fb0f1f7edb75916062c78b1
Juan González <jgonzalez@naver.com>
2021-11-17 15:01:46 +0900

    created

---
+++ src/components/UI/LandingComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <section>
+        <div id="landing-left">
+            <h1>Healty Food For The Good Life</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, nobis!</p>
+            <div>
+                <base-button class="button" buttonTitle="Learn More" buttonType='wholeButton'></base-button>
+                <base-button class="button" buttonTitle="Order Now" buttonType='paleButton'></base-button>
+            </div>
+        </div>
+        <div id="landing-right">
+            <img src="../../assets/chef.png">
+        </div>
+    </section>
+</template>
+
+<style scoped>
+section {
+    display: flex;
+    place-content: center;
+    justify-content: space-between;
+    padding: 0 3rem;
+}
+
+#landing-left{
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+    justify-content: center;
+}
+h1 {
+    color: #256C40;
+    font-size: 3rem;
+    margin: 0
+}
+
+.button {
+    margin: 0 2rem;
+}
+
+img {
+    height: 400px;
+    width: auto;
+}
+</style>
+
