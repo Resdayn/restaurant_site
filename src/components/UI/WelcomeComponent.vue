485c04b11a4fcbdfaf6deec73007f0872032e668
Juan González <jgonzalez@naver.com>
2021-11-17 15:49:23 +0900

    created

---
+++ src/components/UI/WelcomeComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <section>
+        <div id="welcome-left">
+            <img src="../../assets/table.jpg">
+        </div>
+        <div id="welcome-right">
+            <h1>Welcome to the Vegan Restaurant</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero nam ut, assumenda velit repellat consequatur quis fugiat corporis necessitatibus commodi!</p>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+section{
+    display: flex;
+    justify-content: center;
+    height: 400px;
+}
+
+img {
+    max-height: 400px;
+    margin: auto;
+    margin-right: -5rem;
+
+}
+#welcome-right{
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+    justify-content: space-evenly;
+    width: 50vw;
+    background: #256C40;
+    color: white;
+    height: 100%;
+}
+
+#welcome-left{
+    width:50 vw;
+    overflow: hidden;
+}
+
+
+</style>
