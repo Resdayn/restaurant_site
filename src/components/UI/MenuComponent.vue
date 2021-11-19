3f870dadafea12c424cfd7ba8b4dfba7aba69d2d
Juan González <jgonzalez@naver.com>
2021-11-19 15:28:05 +0900

    created

---
+++ src/components/UI/MenuComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <div id="menu-title">
+            <h1>Menu Categories</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptas doloremque reiciendis corporis, quo tempora recusandae quam eligendi nobis sit beatae veritatis vel saepe!</p>
+        </div>
+        <div id="menu-images">
+            <img src="../../assets/menu-categories/menu-category1.png">
+            <img src="../../assets/menu-categories/menu-category2.png">
+            <img src="../../assets/menu-categories/menu-category3.png">
+        </div>
+    </section>
+</template>
+
+<style scoped>
+#menu-title{
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+    justify-content: center;
+}
+h1{
+    font-size: 40px;
+    color: #256C40;
+}
+#menu-images{
+    display: flex;
+    place-items: center;
+    justify-content: center;
+}
+
+
+</style>
