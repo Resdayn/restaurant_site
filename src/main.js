d2a9e43bf80a0c78b9740536d7a0c67872f16936
Juan González <jgonzalez@naver.com>
2021-11-19 17:08:25 +0900

    imported TheFooter

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import TheNav from './components/UI/TheNav.vue'
 import BaseButton from './components/Base/BaseButton';
+import TheFooter from './components/UI/TheFooter.vue'
 
 const app = createApp(App);
 
 app.component('the-nav', TheNav);
 app.component('base-button', BaseButton);
+app.component('the-footer', TheFooter);
 
 app.use(router).mount('#app');
