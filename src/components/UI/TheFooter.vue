8539c69a48532f2dba5969371b1df88edca0e70b
Juan González <jgonzalez@naver.com>
2021-12-08 12:07:34 +0900

    added wrapper for the footer and copyright

--- src/components/UI/TheFooter.vue
+++ src/components/UI/TheFooter.vue
@@ -1,35 +1,36 @@
 <template>
-    <section>
-        <img id="logo" src="../../assets/restaurant_logo.png">
-        <p id="logo-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis necessitatibus soluta assumenda dolores repudiandae odio.</p>
-        <div id="navigation">
-            <h2>Navigation</h2>
-            <h3>Home</h3>
-            <h3>Pages</h3>
-            <h3>About Us</h3>
-            <h3>Services</h3>
+    <div>
+        <section>
+            <img id="logo" src="../../assets/restaurant_logo.png">
+            <p id="logo-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis necessitatibus soluta assumenda dolores repudiandae odio.</p>
+            <div id="navigation">
+                <h2>Navigation</h2>
+                <h3>Home</h3>
+                <h3>Pages</h3>
+                <h3>About Us</h3>
+                <h3>Services</h3>
+            </div>
+            <div id="quickLink">
+                <h2>Contact Us</h2>
+                <h3>FAQs</h3>
+                <h3>Booking</h3>
+                <h3>Pages</h3>
+            </div>
+            <div id="services">
+                <h2>Home</h2>
+                <h3>Contact</h3>
+                <h3>Blog</h3>
+            </div>
+            <p>London Eye, London, United Kingdom</p>
+            <p>088-777-666-85</p>
+            <p>contact@vegan.com</p>
+            <p>ICONS</p>
+        </section>
+        <div id="copyright">
+            <hr>
+            <p>Built by Juan González Palma based on the Vegan Restaurant theme on ThemeForest</p>
         </div>
-        <div id="quickLink">
-            <h2>Contact Us</h2>
-            <h3>FAQs</h3>
-            <h3>Booking</h3>
-            <h3>Pages</h3>
-        </div>
-        <div id="services">
-            <h2>Home</h2>
-            <h3>Contact</h3>
-            <h3>Blog</h3>
-        </div>
-        <p>London Eye, London, United Kingdom</p>
-        <p>088-777-666-85</p>
-        <p>contact@vegan.com</p>
-        <p>ICONS</p>
-    </section>
-    <div id="copyright">
-        <hr>
-        <p>Built by Juan González Palma based on the Vegan Restaurant theme on ThemeForest</p>
     </div>
-    
  
 </template>
 
@@ -47,8 +48,6 @@ section{
     "copyright copyright copyright copyright";
     background-color: #95C18D;
     color: black;
-    position: absolute;
-    bottom: 0;
     width: 100%;
 }
 
