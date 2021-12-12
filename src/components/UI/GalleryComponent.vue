2fbb3b4ffc91b8367cbdc12a7a7bd67c040e9122
Juan Gonzalez <jgonzalez@naver.com>
2021-12-12 11:17:55 +0900

    created

---
+++ src/components/UI/GalleryComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="container">
+        <img class="galleryImg" src="../../assets/gallery/img1.jpg">
+        <img class="galleryImg" src="../../assets/gallery/img2.jpg">
+        <img class="galleryImg" src="../../assets/gallery/img3.jpg">
+        <img class="galleryImg" src="../../assets/gallery/img4.jpg">
+        <img class="galleryImg" src="../../assets/gallery/img5.jpg">
+        <img class="galleryImg" src="../../assets/gallery/img6.jpg">
+        <img class="galleryImg" src="../../assets/gallery/img6.jpg">
+    </section>
+</template>
+
+<style scoped>
+.container{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.galleryImg{
+    height: 200px;
+    width: auto;
+}
+</style>
