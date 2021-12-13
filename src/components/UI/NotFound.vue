d96596674e4394b951880c239d37358756641e4d
Juan Gonzalez <jgonzalez@naver.com>
2021-12-13 11:05:41 +0900

    created

---
+++ src/components/UI/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+<section>
+    <h1>404</h1>
+    <h2>Not Found</h2>
+    <h3>The requested resource could not be found in the server.</h3>
+</section>
+    
+</template>
+
+<style scoped>
+section {
+    padding:3rem 0;
+}
+h1, h2, h3 {
+    padding: 2rem 0;
+}
+h1 {
+    font-size: 55px;
+}
+h2 {
+    font-size: 48px;
+}
+h3 {
+    font-size: 40px;
+}
+</style>
